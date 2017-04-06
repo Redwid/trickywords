3ce741fce11d3b224ac4a5a340147fbaee291ea0
Redwid <redwid@gmail.com>
2017-04-06 11:02:43 +0100

    Added failed list.

--- app/src/main/java/trickywords/android/redwid/org/trickywords/FullscreenActivity.java
+++ app/src/main/java/trickywords/android/redwid/org/trickywords/FullscreenActivity.java
@@ -2,7 +2,11 @@ package trickywords.android.redwid.org.trickywords;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,17 +24,23 @@ public class FullscreenActivity extends AppCompatActivity {
     private int mCorrectCounter = 0;
     private int mIndex = 0;
     private final List<String> mWords = new ArrayList<>();
+    private final List<String> mFailedWords = new ArrayList<>();
     private TextView mWordTextView;
     private TextView mFailStatsView;
     private TextView mCorrectStatsView;
     private Button mFailButton;
     private Button mCorrectButton;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_fullscreen);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycle_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         mFailButton = (Button) findViewById(R.id.fail_button);
         mCorrectButton = (Button) findViewById(R.id.correct_button);
@@ -50,23 +60,69 @@ public class FullscreenActivity extends AppCompatActivity {
     public void onFailButtonClick(final View v) {
         mFailedCounter++;
         mFailStatsView.setText(Integer.toString(mFailedCounter));
-        nextWord();
+        mFailedWords.add(mWordTextView.getText().toString());
+        doNextWord();
     }
 
     public void onCorrectButtonClick(final View v) {
         mCorrectCounter++;
         mCorrectStatsView.setText(Integer.toString(mCorrectCounter));
-        nextWord();
+        doNextWord();
     }
 
-    private void nextWord() {
+    private void doNextWord() {
         mIndex++;
         if(mIndex > mWords.size() - 1) {
-            mFailButton.setVisibility(View.GONE);
-            mCorrectButton.setVisibility(View.GONE);
+            doFinish();
         }
         else {
             mWordTextView.setText(mWords.get(mIndex));
+        }
+    }
+
+    private void doFinish() {
+        if(mFailedWords.size() != 0) {
+            mWordTextView.setVisibility(View.GONE);
+            mRecyclerView.setAdapter(new FailedWordsAdapter(mFailedWords));
+        }
+
+        mFailButton.setVisibility(View.GONE);
+        mCorrectButton.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    protected class FailedWordsAdapter extends RecyclerView.Adapter<FailedWordsAdapter.ViewHolder> {
+
+        protected class ViewHolder extends RecyclerView.ViewHolder {
+
+            protected TextView mTextView;
+            public ViewHolder(View view) {
+                super(view);
+                mTextView = (TextView) view.findViewById(R.id.title);
+            }
+        }
+
+        private final List<String> mData;
+
+        public FailedWordsAdapter(List<String> data) {
+            mData = data;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+            final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+            final ViewHolder viewHolder = new ViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+            holder.mTextView.setText(mData.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
         }
     }
 }
